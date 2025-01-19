f2be777f70a07e1ed20963ef65d5c3d1052e006b
Anshm1234 <142911652+Anshm1234@users.noreply.github.com>
2025-01-19 22:27:10 +0530

    Add files via upload

---
+++ assignemenyt_2/ques5.py
@@ -0,0 +1,9 @@
+sample_dict={
+    "name":"Ansh madaan",
+    "age":19,
+    "Roll_no":102317159,
+    "city":"Sri Muktsar sahib"
+}
+sample_dict["Location"]=sample_dict.pop("city")
+print(sample_dict)
+print("\n")
